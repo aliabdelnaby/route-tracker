9ff641be35f021c065955e8130dc036c4e3a7ada
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-06-26 00:04:07 +0300

    Create get predictions method [2]

--- lib/utils/google_maps_place_service.dart
+++ lib/utils/google_maps_place_service.dart
@@ -1,7 +1,26 @@
+import 'dart:convert';
+
 import 'package:route_tracker/models/place_autocomplete_model/place_autocomplete_model.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleMapsPlaceService {
+  final String baseUrl = 'https://maps.googleapis.com/maps/api/place';
+  final String apiKey = 'AIzaSyB7cXZs7rYf0S9yBpZn0q9QpKfU5CpK5z8';
   Future<List<PlaceModel>> getPredictions({required String input}) async {
-    return [];
+    var response = await http.get(
+      Uri.parse(
+        '$baseUrl/autocomplete/json?key=$apiKey&input=$input',
+      ),
+    );
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['predictions'];
+      List<PlaceModel> places = [];
+      for (var item in data) {
+        places.add(PlaceModel.fromJson(item));
+      }
+      return places;
+    } else {
+      throw Exception();
+    }
   }
 }
