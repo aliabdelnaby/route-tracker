12e997abefb0f8f61640333c499dabff1b2bb1a2
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-06-25 14:24:21 +0300

    Create Project

---
+++ lib/views/google_map_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapView extends StatefulWidget {
+  const GoogleMapView({super.key});
+
+  @override
+  State<GoogleMapView> createState() => _GoogleMapViewState();
+}
+
+class _GoogleMapViewState extends State<GoogleMapView> {
+  late GoogleMapController mapController;
+  late CameraPosition initialCameraPosition;
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+      target: LatLng(0.0, 0.0),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      zoomControlsEnabled: false,
+      initialCameraPosition: initialCameraPosition,
+    );
+  }
+}
