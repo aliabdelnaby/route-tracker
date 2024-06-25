9ffbe3dbcccdeaf0786c64ccdae2829e5eb31fea
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-06-25 23:42:35 +0300

    Create get predictions method

---
+++ lib/utils/google_maps_place_service.dart
@@ -0,0 +1,7 @@
+import 'package:route_tracker/models/place_autocomplete_model/place_autocomplete_model.dart';
+
+class GoogleMapsPlaceService {
+  Future<List<PlaceModel>> getPredictions({required String input}) async {
+    return [];
+  }
+}
