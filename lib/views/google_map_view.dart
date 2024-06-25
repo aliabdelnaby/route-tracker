7fd617641cb73f2b9ba5a74d9462d5c864c45715
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-06-25 15:31:27 +0300

    Update Current Location

--- lib/views/google_map_view.dart
+++ lib/views/google_map_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:route_tracker/utils/location_service.dart';
 
 class GoogleMapView extends StatefulWidget {
   const GoogleMapView({super.key});
@@ -11,19 +12,55 @@ class GoogleMapView extends StatefulWidget {
 class _GoogleMapViewState extends State<GoogleMapView> {
   late GoogleMapController mapController;
   late CameraPosition initialCameraPosition;
+  late LocationServices locationService;
+  Set<Marker> markers = {};
+
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
-      target: LatLng(0.0, 0.0),
+      target: LatLng(26.839663247801774, 29.71044929719575),
     );
+    locationService = LocationServices();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      markers: markers,
       zoomControlsEnabled: false,
       initialCameraPosition: initialCameraPosition,
+      onMapCreated: (controller) {
+        mapController = controller;
+        updateCurrentLocation();
+      },
     );
+  }
+
+  void updateCurrentLocation() async {
+    try {
+      var locationData = await locationService.getCurrentLocation();
+      var myLocationMarker = Marker(
+        markerId: const MarkerId("myLocation"),
+        position: LatLng(locationData.latitude!, locationData.longitude!),
+      );
+
+      mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(locationData.latitude!, locationData.longitude!),
+            zoom: 15,
+          ),
+        ),
+      );
+      markers.add(myLocationMarker);
+      setState(() {});
+    } on LocationServiceException catch (e) {
+      // TODO
+    } on LocationPermissionException catch (e) {
+      // TODO
+    } catch (e) {
+      // TODO
+    }
   }
 }
