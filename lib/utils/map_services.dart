6a5a5d3e5db174277a327b70cc8e3667a9cf604f
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-07-03 17:54:55 +0300

    Refactor code [1]

--- lib/utils/map_services.dart
+++ lib/utils/map_services.dart
@@ -1,3 +1,4 @@
+import 'package:route_tracker/models/place_autocomplete_model/place_autocomplete_model.dart';
 import 'package:route_tracker/utils/google_maps_place_service.dart';
 import 'package:route_tracker/utils/location_service.dart';
 import 'package:route_tracker/utils/routes_service.dart';
@@ -6,4 +7,21 @@ class MapServices {
   PlaceService placeService = PlaceService();
   LocationServices locationService = LocationServices();
   RoutesService routesService = RoutesService();
+
+  getPredictions({
+    required String input,
+    required String sessiontoken,
+    required List<PlaceModel> places,
+  }) async {
+    if (input.isNotEmpty) {
+      var result = await placeService.getPredictions(
+        input: input,
+        sessiontoken: sessiontoken,
+      );
+      places.clear();
+      places.addAll(result);
+    } else {
+      places.clear();
+    }
+  }
 }
